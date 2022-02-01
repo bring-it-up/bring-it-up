1d40a830e22a2800713f24f63f37e1fc1c20acf5
samanthahsu <shsu877@gmail.com>
2022-02-01 12:41:39 -0800

    api cors

--- server/index.ts
+++ server/index.ts
@@ -7,13 +7,30 @@ const app: express.Application = express();
 // Take a port 3000 for running server.
 const port: number = 4000;
 
+// setup cors to allow client http access
+const cors = require('cors');
+app.use(cors({
+  origin: '*'
+}))
+
+// test data to serve
+const groceries = [
+  {
+    "id": "1",
+    "item": "Cauliflower"
+  },
+  {
+    "id": "2",
+    "item": "Eggs"
+  }
+]
+
 // Handling '/' Request
 app.get('/', (_req, _res) => {
-	_res.send("HELLO!!!ee");
+	_res.send(groceries);
 });
 
 // Server setup
 app.listen(port, () => {
-	console.log(`TypeScript with Express
-		http://localhost:${port}/`);
+	console.log(`TypeScript with Express http://localhost:${port}/`);
 });
