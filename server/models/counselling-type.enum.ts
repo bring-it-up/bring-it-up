d1bd06fe5e905362af74f55b50171961adc9f582
angusho1 <57027339+angusho1@users.noreply.github.com>
2022-06-04 21:18:32 -0700

    Add enum types for data model

---
+++ server/models/counselling-type.enum.ts
@@ -0,0 +1,7 @@
+export enum CounsellingType {
+    MEDICAL = 'Medical',
+    COUNSELLING = 'Counselling',
+    CRISIS = 'Crisis Line',
+    WELLNESS = 'Wellness',
+    WORKSHOP = 'Workshop'
+}
---
+++ server/models/urgency-level.enum.ts
@@ -0,0 +1,5 @@
+export enum UrgencyLevel {
+    CRISIS = 'Crisis',
+    SHORT_TERM = 'Short Term',
+    LONG_TERM = 'Long Term'
+}
