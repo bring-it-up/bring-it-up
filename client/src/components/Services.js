7077cfd1305080537b825cb94c18f7997f404ec8
Michelle Kim <yeojin011016@gmail.com>
2022-06-26 12:34:23 -0700

    set up examplary data fetch from backend in services.js

--- client/src/components/Services.js
+++ client/src/components/Services.js
@@ -1,6 +1,32 @@
+import { useState, useEffect } from 'react';
+
+function GetData() {
+    var [data, setData] = useState([]);
+    var serviceNames = [];
+  
+    // this returns array of all service objects
+    useEffect(() => {
+        fetch("http://localhost:4000/counselling-services")
+        .then(res => res.json())
+        .then(temp => console.log(temp))
+        .then(parsedData => setData(parsedData))
+        .catch((e) => console.log(e));
+    }, [])
+  
+    for (let service of data) {
+      serviceNames.push(service.serviceName);
+    }
+    console.log(serviceNames);
+    return serviceNames;
+  }
+  
+
 const Services = () => {
     return (
-        <h1>Services</h1>
+        <>
+            <h1>Services</h1>
+            <p>services: {GetData()}</p>
+        </>
     );
 }
 
