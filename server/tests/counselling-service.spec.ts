a7f15e0e5009252eb7c6326b9864773298b411bf
angusho1 <57027339+angusho1@users.noreply.github.com>
2022-07-09 15:19:30 -0700

    Write initial server integration tests

--- server/tests/counselling-service.spec.ts
+++ server/tests/counselling-service.spec.ts
@@ -2,6 +2,7 @@ import chai from 'chai';
 import chaiHttp from 'chai-http';
 import server from '../index';
 import { CounsellingService } from '../models/counsellingService.model';
+import { service1Data, service2Data } from './data';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const should = chai.should();
 
@@ -10,6 +11,8 @@ chai.use(chaiHttp);
 describe('Counselling Services', () => {
     beforeEach(async () => {
         await CounsellingService.remove({});
+        const service = new CounsellingService(service1Data);
+        await service.save();
     });
 
     describe('GET /counselling-services', () => {
@@ -18,7 +21,26 @@ describe('Counselling Services', () => {
                 .get('/counselling-services');
             result.should.have.status(200);
             result.body.should.be.a('array');
-            result.body.length.should.be.eql(0);
+            result.body.length.should.be.eql(1);
+        });
+    });
+
+    describe('POST /counselling-services', () => {
+        it('should create a counselling service', async () => {
+            const result = await chai.request(server)
+                .post('/counselling-services')
+                .send(service2Data);
+            result.should.have.status(201);
+        });
+
+        it('should reject duplicate services', async () => {
+            await chai.request(server)
+                .post('/counselling-services')
+                .send(service2Data);
+            const result = await chai.request(server)
+                .post('/counselling-services')
+                .send(service2Data);
+            result.should.have.status(400);
         });
     });
 });
---
+++ server/tests/data.ts
@@ -0,0 +1,28 @@
+export const service1Data = {
+    serviceName: "Student Assistance Program (SAP)",
+    school: "UBC",
+    organization: "Aspiria",
+    serviceType: ["Counselling"],
+    urgency: "Immediate",
+    targetClients: ["UBC students"],
+    isAllDay: true,
+    website: "https://students.ubc.ca/health/ubc-student-assistance-program-sap",
+    specialty: ["Stress", "Anxiety"],
+    delivery: ["Online","Phone"],
+    description: "Offered by Aspiria, the UBC Student Assistance Program (SAP) is a free, 24/7 wellness resource for students. Services include personal counselling, life coaching, group programs and more based on your needs."
+};
+
+export const service2Data = {
+    serviceName: "My SSP",
+    school: "SFU",
+    organization: "Morneau Shepell (LifeWorks)",
+    serviceType: ["Counselling"],
+    urgency: "Immediate",
+    targetClients: ["SFU students","FIC students"],
+    isAllDay: true,
+    website: "https://www.sfu.ca/students/health/get-support/my-ssp.html",
+    specialty: ["Stress","Anxiety","Homesickness","Cultural Adjustment"],
+    isOfferedOnline: true,
+    delivery: ["Online","Phone","App"],
+    description: "My SSP (My Student Support Program) is an app dedicated to supporting SFU/FIC students through its 24/7 crisis line, short-term counselling appointments and relevant resources on a range of mental health topics."
+};
