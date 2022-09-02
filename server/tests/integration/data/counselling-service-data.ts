8b0a324187eaaa1b26d78b3a6d266abe47ce4b9b
shiven-shukla <shivenshukla03@gmail.com>
2022-09-01 22:15:21 -0700

    [BE#39] adds integrations tests for search, get, and del

--- server/tests/integration/data/counselling-service-data.ts
+++ server/tests/integration/data/counselling-service-data.ts
@@ -9,7 +9,8 @@ export const service1Data = {
     website: "https://students.ubc.ca/health/ubc-student-assistance-program-sap",
     specialty: ["Stress", "Anxiety"],
     delivery: ["Online","Phone"],
-    description: "Offered by Aspiria, the UBC Student Assistance Program (SAP) is a free, 24/7 wellness resource for students. Services include personal counselling, life coaching, group programs and more based on your needs."
+    description: "Offered by Aspiria, the UBC Student Assistance Program (SAP) is a free, 24/7 wellness resource for students. Services include personal counselling, life coaching, group programs and more based on your needs.",
+    secondaryID: "student-assistance-program-sap"
 };
 
 export const service2Data = {
@@ -24,5 +25,22 @@ export const service2Data = {
     specialty: ["Stress","Anxiety","Homesickness","Cultural Adjustment"],
     isOfferedOnline: true,
     delivery: ["Online","Phone","App"],
-    description: "My SSP (My Student Support Program) is an app dedicated to supporting SFU/FIC students through its 24/7 crisis line, short-term counselling appointments and relevant resources on a range of mental health topics."
+    description: "My SSP (My Student Support Program) is an app dedicated to supporting SFU/FIC students through its 24/7 crisis line, short-term counselling appointments and relevant resources on a range of mental health topics.",
+    secondaryID: "my-ssp"
 };
+
+// secondaryID excluded because this data will be used for testing POST which generates the ID
+export const service3Data = {
+    serviceName: "UBC Psychology Clinic",
+    school: "UBC",
+    organization: "UBC Psychology",
+    serviceType: ["Counselling"],
+    urgency: "Same Month",
+    targetClients: ["Adults in Greater Vancouver"],
+    isAllDay: false,
+    website: "https://clinic.psych.ubc.ca/services/",
+    specialty: ["Stress", "Anxiety", "Depression"],
+    isOfferedOnline: false,
+    delivery: ["In Person"],
+    description: "The UBC Psychology Clinic offers comprehensive psychological services adults. Our clinical services are provided by graduate students from UBC’s doctoral programme in clinical psychology. Student clinicians are closely supervised by Registered Psychologists from our faculty and the community.Our Clinic is not able to provide psychiatric or emergency services or services where there is a risk of frequent or severe crisis or involvement with the law."
+}
