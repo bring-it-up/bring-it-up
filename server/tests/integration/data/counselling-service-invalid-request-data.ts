481255229794f0352f3086fa9730244dd390ff89
shiven-shukla <shivenshukla03@gmail.com>
2022-09-05 00:21:55 -0700

    [NO-ISSUE] adds missing data

--- server/tests/integration/data/counselling-service-invalid-request-data.ts
+++ server/tests/integration/data/counselling-service-invalid-request-data.ts
@@ -132,3 +132,97 @@ export const invalidSpecialtyData = {
     delivery: ["Online","Phone","App"],
     description: "My SSP (My Student Support Program) is an app dedicated to supporting SFU/FIC students through its 24/7 crisis line, short-term counselling appointments and relevant resources on a range of mental health topics."
 };
+
+export const invalidHourData = {
+    serviceName: "My SSP",
+    school: "SFU",
+    organization: "Morneau Shepell (LifeWorks)",
+    serviceType: ["Counselling"],
+    urgency: "Immediate",
+    targetClients: ["SFU students","FIC students"],
+    isAllDay: true,
+    website: "https://www.sfu.ca/students/health/get-support/my-ssp.html",
+    specialty: ["Stress","Anxiety","Cultural Adjustment"],
+    isOfferedOnline: true,
+    delivery: ["Online","Phone","App"],
+    description: "My SSP (My Student Support Program) is an app dedicated to supporting SFU/FIC students through its 24/7 crisis line, short-term counselling appointments and relevant resources on a range of mental health topics.",
+    hours: {
+        mon: [9.00,  22.30],
+        tue: [1],
+        wed: [10.30, 20.00],
+        thu: [1],
+        fri: [7.15, 20.60],
+        sat: [0],
+        sun: [0]
+    }
+};
+
+export const invalidSpecialHourData = {
+    serviceName: "My SSP",
+    school: "SFU",
+    organization: "Morneau Shepell (LifeWorks)",
+    serviceType: ["Counselling"],
+    urgency: "Immediate",
+    targetClients: ["SFU students","FIC students"],
+    isAllDay: true,
+    website: "https://www.sfu.ca/students/health/get-support/my-ssp.html",
+    specialty: ["Stress","Anxiety","Cultural Adjustment"],
+    isOfferedOnline: true,
+    delivery: ["Online","Phone","App"],
+    description: "My SSP (My Student Support Program) is an app dedicated to supporting SFU/FIC students through its 24/7 crisis line, short-term counselling appointments and relevant resources on a range of mental health topics.",
+    hours: {
+        mon: [9.00,  22.30],
+        tue: [1],
+        wed: [10.30, 20.00],
+        thu: [1],
+        fri: [7.15, 8.15],
+        sat: [2],
+        sun: [0]
+    }
+};
+
+export const invalidNumHours = {
+    serviceName: "My SSP",
+    school: "SFU",
+    organization: "Morneau Shepell (LifeWorks)",
+    serviceType: ["Counselling"],
+    urgency: "Immediate",
+    targetClients: ["SFU students","FIC students"],
+    isAllDay: true,
+    website: "https://www.sfu.ca/students/health/get-support/my-ssp.html",
+    specialty: ["Stress","Anxiety","Cultural Adjustment"],
+    isOfferedOnline: true,
+    delivery: ["Online","Phone","App"],
+    description: "My SSP (My Student Support Program) is an app dedicated to supporting SFU/FIC students through its 24/7 crisis line, short-term counselling appointments and relevant resources on a range of mental health topics.",
+    hours: {
+        mon: [9.00,  22.30],
+        tue: [1],
+        fri: [7.15, 8.15],
+        sat: [2],
+        sun: [0]
+    }
+};
+
+export const invalidDay = {
+    serviceName: "My SSP",
+    school: "SFU",
+    organization: "Morneau Shepell (LifeWorks)",
+    serviceType: ["Counselling"],
+    urgency: "Immediate",
+    targetClients: ["SFU students", "FIC students"],
+    isAllDay: true,
+    website: "https://www.sfu.ca/students/health/get-support/my-ssp.html",
+    specialty: ["Stress", "Anxiety", "Cultural Adjustment"],
+    isOfferedOnline: true,
+    delivery: ["Online", "Phone", "App"],
+    description: "My SSP (My Student Support Program) is an app dedicated to supporting SFU/FIC students through its 24/7 crisis line, short-term counselling appointments and relevant resources on a range of mental health topics.",
+    hours: {
+        mon: [9.00, 22.30],
+        tuesday: [1],
+        wed: [10.30, 20.00],
+        thu: [1],
+        fri: [7.15, 8.15],
+        sat: [0],
+        sun: [0]
+    }
+};
