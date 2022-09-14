66ca468f4027418d3f84e87c726b045472e67ca1
michelleykim <yeojin011016@gmail.com>
2022-09-14 00:03:35 -0700

    code formatting fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,28 @@
-import './App.css';
+import "./App.css";
 import Navigation from "./components/Navigation";
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import About from './components/About';
-import Services from './components/Services';
-import Home from './components/Home';
-import { ReactElement } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import About from "./components/About";
+import Services from "./components/Services";
+import Home from "./components/Home";
+import { ReactElement } from "react";
 
 function App(): ReactElement {
-  return (
-    <div>
-    <div>
-      {/* for navbar */}
-      <Router>
-        <Navigation />
-        <Switch>
-          <Route exact path='/' component={Home}/>
-          <Route exact path='/about' component={About}/>
-          <Route exact path='/services' component={Services} />
-        </Switch>
-      </Router>
-      {/* end navbar */} 
-      </div>
-  </div>
-  );
+	return (
+		<div>
+			<div>
+				{/* for navbar */}
+				<Router>
+					<Navigation />
+					<Switch>
+						<Route exact path="/" component={Home} />
+						<Route exact path="/about" component={About} />
+						<Route exact path="/services" component={Services} />
+					</Switch>
+				</Router>
+				{/* end navbar */}
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- client/src/components/Services.tsx
+++ client/src/components/Services.tsx
@@ -1,43 +1,43 @@
-import { ReactElement, useState, useEffect } from 'react';
-import Service from '../Service';
-import ServiceCard from './ServiceCard';
+import { ReactElement, useState, useEffect } from "react";
+import Service from "../Service";
+import ServiceCard from "./ServiceCard";
 
 function GetFirstService(): Service {
-    var [data, setData] = useState<any[]>([]);
-  
-    useEffect(() => {
-        const controller = new AbortController();
-        const signal = controller.signal;
+	var [data, setData] = useState<any[]>([]);
 
-        fetch("http://localhost:4000/counselling-services", {signal: signal})
-        .then(res => res.json())
-        .then(parsedData => setData(parsedData))
-        .catch((e) => console.log(e));
-        
-        // abort fetch request if we're no longer on services page
-        return () => {
-            controller.abort();
-        }
-    }, []);
+	useEffect(() => {
+		const controller = new AbortController();
+		const signal = controller.signal;
 
-    // we need this conditional render since html render might happen before setData above
-    // more info: https://www.reddit.com/r/learnreactjs/comments/sgim34/fetch_api_works_then_suddenly_stops_working_after/
-    if (data && data.length >= 1) {
-        var stringJson: string = JSON.stringify(data[0]);
+		fetch("http://localhost:4000/counselling-services", { signal: signal })
+			.then((res) => res.json())
+			.then((parsedData) => setData(parsedData))
+			.catch((e) => console.log(e));
 
-        return JSON.parse(stringJson);
-    }
+		// abort fetch request if we're no longer on services page
+		return () => {
+			controller.abort();
+		};
+	}, []);
 
-    return new Service();
-} 
+	// we need this conditional render since html render might happen before setData above
+	// more info: https://www.reddit.com/r/learnreactjs/comments/sgim34/fetch_api_works_then_suddenly_stops_working_after/
+	if (data && data.length >= 1) {
+		var stringJson: string = JSON.stringify(data[0]);
+
+		return JSON.parse(stringJson);
+	}
+
+	return new Service();
+}
 
 const Services = (): ReactElement => {
-    return (
-        <>
-            <h1>Services</h1>
-            <ServiceCard service={GetFirstService()}/>
-        </>
-    );
-}
+	return (
+		<>
+			<h1>Services</h1>
+			<ServiceCard service={GetFirstService()} />
+		</>
+	);
+};
 
 export default Services;
